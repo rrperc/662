d75606cfb8e809d4f3eb2ba09189eb4ad81a1613
Jose81924 <115500208+Jose81924@users.noreply.github.com>
2022-10-13 12:46:07 -0700

    scripts

---
+++ 663/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        GetComponent<Animator>().SetFloat("xInput", xInput);
+        GetComponent<Animator>().SetFloat("yInput", yInput);
+        Debug.Log(xInput);
+        Vector2 moveDir = new Vector2(xInput, yInput);
+        GetComponent<Rigidbody2D>().velocity = moveDir * speed;
+    }
+}
