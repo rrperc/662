561d964906e5934a3128401fccee8b26eb6937f7
Jose81924 <115500208+Jose81924@users.noreply.github.com>
2022-10-18 13:58:24 -0700

    Update PlayerHealth.cs

--- 663/Assets/Player/PlayerHealth.cs
+++ 663/Assets/Player/PlayerHealth.cs
@@ -39,4 +39,26 @@ public class PlayerHealth : MonoBehaviour
             }
         }
     }
+    public void TakeDamage(float _Damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _Damage, 0, startingHealth);
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("Hurt");
+            //iframes
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
 }
