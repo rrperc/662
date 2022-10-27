6a3eaff932a87d88467c5272765e5146df7df394
Jose81924 <115500208+Jose81924@users.noreply.github.com>
2022-10-27 12:17:31 -0700

    Fixed Player Shoot

--- 663/Assets/Player/PlayerShoot.cs
+++ 663/Assets/Player/PlayerShoot.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [SerializeField]
+    private Transform barrel;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private GameObject[] ammo;
+
+    private int ammoAmount;
+    //Start is called before the first frame update
+     void Start()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            ammo[i].gameObject.SetActive(false);
+        }
+        ammoAmount = 0;
+    }
     public GameObject prefab;
     public float bulletSpeed = 10.0f;
     public float bulletLifetime = 2.0f;
@@ -13,8 +32,10 @@ public class PlayerShoot : MonoBehaviour
     void Update()
     {
         //when the mouse is clicked
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && ammoAmount > 0)
         {
+            var spwnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+            spwnedBullet.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 500);
             GameObject bullet = Instantiate(prefab, transform.position, Quaternion.identity);
             Vector3 mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
